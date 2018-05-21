d5fd8568d1cf240cde8f38451bba52b159ae86ed
Ola Awodipe <ion.ca.ola@gmail.com>
2018-05-21 16:56:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function theBeatlesPlay(BeatlesLoops) {
+  const facts = ['John Lennon plays Guitar,''Paul McCartney plays Bass Guitar,''George Harrison plays Lead Guitar,''Ringo Starr plays Drums'];
+  return facts;
+}
+
+function johnlennonFacts(BeatlesLoops) {
+  
+}
+
