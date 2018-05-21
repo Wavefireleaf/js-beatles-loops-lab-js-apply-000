c023ed6a58b849b250e3ebb4921ec43fe33d1422
Ola Awodipe <ion.ca.ola@gmail.com>
2018-05-21 17:30:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function theBeatlesPlay(BeatlesLoops) {
+  
   var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
   var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   for (instruments = 0; instruments < 4; instruments++); {
-    return musicians  'plays'  instruments;
+    return [musicians] 'plays' [instruments];
   }
 }
 
