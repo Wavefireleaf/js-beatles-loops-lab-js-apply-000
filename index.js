e4477503204b788d4fa1bdd5278ac3960b09ee8f
Ola Awodipe <ion.ca.ola@gmail.com>
2018-05-21 18:40:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnlennonFacts(facts) {
   var array = [];
+  var i = 0;
   while (i < facts.length) {
     array.push(facts[i]+"!!!");
     i++;
