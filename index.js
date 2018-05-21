cbefac0b9dd69d684a7824f948d0b587fc998359
Ola Awodipe <ion.ca.ola@gmail.com>
2018-05-21 18:08:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
-    array.push("plays")
+    array.push(musicians[0]+"plays"+instruments[0]);
   }
   return array;
 }
